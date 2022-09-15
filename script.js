88d4d72ea09ce7d52cc61944d326b33ea2263f2d
Madisen Vogel <madisen@Admins-Mac-mini.btm.private>
2022-09-15 13:51:36 -0600

    reformatted date

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 var currentDayEl = document.getElementById('currentDay');
 
 // handles date
-function displayDay() {
-    var today = moment().format('MMM DD, YYYY');
-    currentDayEl.textContent = today;
-  }
-displayDay();
+var today = moment().format('dddd, MMM Do, YYYY');
+currentDayEl.textContent = today;
+
 
 
