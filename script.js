6096a274041fe2b1c6da38a89c205959764301a0
Madisen <madisenvogel@gmail.com>
2022-09-17 19:50:19 -0600

    added timeblocks

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var currentDayEl = document.getElementById('currentDay');
 
 // handles date
-var today = moment().format('dddd, MMM Do, YYYY');
+var today = moment().format('dddd, MMMM Do, YYYY');
 currentDayEl.textContent = today;
 
 
