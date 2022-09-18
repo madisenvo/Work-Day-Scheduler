241dcc56ab51bf1310a56c36f6354c33f378e29e
Madisen <madisenvogel@gmail.com>
2022-09-18 10:39:50 -0600

    local storage working

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 var currentDayEl = document.getElementById('currentDay');
 
-// handles date
-var today = moment().format('dddd, MMMM Do, YYYY');
+// sets date
+var today = moment().format('dddd, MMMM Do');
 currentDayEl.textContent = today;
 
+// saveBtn click listener saves to local storage
+$(".saveBtn").on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    console.log(time);
+    localStorage.setItem(time, text);
+})
 
+// get entry from local storage
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
 
