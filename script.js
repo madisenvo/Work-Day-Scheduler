7f82c40567cc934321c48d116d0b1687def8edcd
Madisen <madisenvogel@gmail.com>
2022-09-18 13:30:44 -0600

    color coding working

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-var currentDayEl = document.getElementById('currentDay');
-
 // sets date
+var currentDayEl = document.getElementById('currentDay');
 var today = moment().format('dddd, MMMM Do');
 currentDayEl.textContent = today;
 
-// saveBtn click listener saves to local storage
-$(".saveBtn").on("click", function(){
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    console.log(time);
-    localStorage.setItem(time, text);
+$(document).ready(function () {
+    // saveBtn click listener saves to local storage
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(time, text);
+    })
+
+    // tracks current time and sets color
+    function setTimeColor() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id"));
+
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+// gets inputs from local storage
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+
+    setTimeColor();
 })
-
-// get entry from local storage
-$("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#1 .description").val(localStorage.getItem("1"));
-$("#2 .description").val(localStorage.getItem("2"));
-$("#3 .description").val(localStorage.getItem("3"));
-$("#4 .description").val(localStorage.getItem("4"));
-$("#5 .description").val(localStorage.getItem("5"));
-
